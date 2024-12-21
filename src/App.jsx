48ffc8d22cfdd6145e3579acae5aba13676b291a
unknown <sujoydaspc2023@gmail.com>
2024-12-21 20:00:23 +0600

    these are two api link

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
       <h1>Hello i am Phone Hunter Api er React Virsion</h1>
       <h1>I want to use 2 api link to load data using react route loader</h1>
 
+      <h1>https://openapi.programming-hero.com/api/phones?search=a </h1>
+      <h1>https://openapi.programming-hero.com/api/phone/oppo_find_x5_pro-11236</h1>
     </>
   )
 }
