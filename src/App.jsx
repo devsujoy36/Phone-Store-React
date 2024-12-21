7388159cb10c0053e985610bc367f564fd659bde
unknown <sujoydaspc2023@gmail.com>
2024-12-21 19:59:22 +0600

    i want o use 2 api

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <>
       <h1>Hello i am Phone Hunter Api er React Virsion</h1>
+      <h1>I want to use 2 api link to load data using react route loader</h1>
+
     </>
   )
 }
